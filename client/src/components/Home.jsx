7002d010e444efdc269eac62e9af512168833579
Aryan <aryanpundir0606@gmail.com>
2023-09-23 00:05:48 +0530

    not needed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -21,20 +21,20 @@ const Index = () => {
 
     <div className='grid gap-8 grid-cols-2 lg:grid-cols-4 md:grid-cols-3 mt-8'>
       {places.length > 0 && places.map((place, index) => (
-        <PlaceCard place={place} index={index}/>
-        // <Link to={"/places/" + place._id} key={index}>
-        // <div className='bg-gray-200 mb-4 rounded-2xl flex '>
-        //   {place.photos?.[0] && (
-        //     <img className="rounded-2xl object-cover aspect-square" src={place.photos[0]} alt="" />
-        //   )}
-        // </div>
-        // <h2 className="font-bold">{place.address}</h2>
-        // <h3 className='text-sm truncate text-gray-500'>{place.title}</h3>
-        // <div className='mt-1'>
-        //   <span className='font-bold'>₹{place.price}</span> per night
-        // </div>
+        //<PlaceCard place={place} index={index}/>
+        <Link to={"/places/" + place._id} key={index}>
+        <div className='bg-gray-200 mb-4 rounded-2xl flex '>
+          {place.photos?.[0] && (
+            <img className="rounded-2xl object-cover aspect-square" src={place.photos[0]} alt="" />
+          )}
+        </div>
+        <h2 className="font-bold">{place.address}</h2>
+        <h3 className='text-sm truncate text-gray-500'>{place.title}</h3>
+        <div className='mt-1'>
+          <span className='font-bold'>₹{place.price}</span> per night
+        </div>
 
-        // </Link>
+        </Link>
       ))}
 
 
