e483f09a093baccad3b5321bb87cdec13135cf7e
Gaurav-2812 <gauravkumar21122003@gmail.com>
2023-09-23 16:56:01 +0530

    footer

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Foot from './components/Foot'
 
 const Layout = () => {
   return (
     <div className=' font-primary flex flex-col min-h-screen'>
       <Navbar/>
-      <Outlet/>
+      <div className="mx-auto bg-white flex min-h-screen max-w-screen-xxl flex-col">
+        <Outlet />
+      </div>
+      <Foot />
     </div>
   )
 }
